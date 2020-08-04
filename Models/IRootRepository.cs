8f148683215b5bfaf66650754cd521557fb1ed3e
punam.edominer@gmail.com <punam.edominer@gmail.com>
2020-08-05 01:20:21 +0530

    Add project files.

---
+++ Models/IRootRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCM.Models
+{
+    public interface IRootRepository
+    {
+        void Insert<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+    }
+}
---
+++ Models/SQLRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity.Migrations;
+
+namespace PCM.Models
+{
+    public class SQLRepository : IRootRepository
+    {
+        private AppDbContext _context;
+        public SQLRepository(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void Insert<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Update(entity);
+        }
+        
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            bool blnSave = false;
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    blnSave = await _context.SaveChangesAsync() > 0;
+                    transaction.Commit();
+                    return blnSave;
+                }
+                catch(Exception ex)
+                {
+                    transaction.Rollback();
+                    return false;
+                }
+            }
+        }
+    }
+}
